9e48a28fdf9b27dd3fea5b0ee9396ce93baf2163
Sebastian Nugnez <sebastian.nugnez@gmail.com>
2017-03-21 14:10:20 +0800

    ckill controller CRUD done

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -16,7 +16,6 @@ class SkillsController < ApplicationController
     skill_params = params.require(:skill).permit(:name, :price, :location, :category, :description)
     @skill = Skill.new(skill_params)
     @skill.user_id = current_user.id
-
     if @skill.save
       redirect_to skill_path(@skill)
     else
@@ -25,12 +24,19 @@ class SkillsController < ApplicationController
   end
 
   def edit
+    @skill = Skill.find(params[:id])
   end
 
   def update
+    skill_params = params.require(:skill).permit(:name, :price, :location, :category, :description)
+    @skill = Skill.find(params[:id])
+    @skill.update(skill_params)
+    redirect_to skill_path(@skill)
   end
 
   def destroy
+    @skill = Skill.find(params[:id])
+    @skill.destroy
   end
 
 end

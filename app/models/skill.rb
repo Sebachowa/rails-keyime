a3d6eef79c30f924dde3d407da3eceb233e27ab2
Sebastian Nugnez <sebastian.nugnez@gmail.com>
2017-03-22 15:56:53 +0800

    first styling of the index;

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,11 +1,12 @@
 class Skill < ApplicationRecord
+  CATEGORIES = ["Design", "Marketing", "Writing", "Video", "Music", "Programming", "Tech", "Bussiness", "Lifestyle"]
   has_attachments :photos, maximum: 2
   belongs_to :user
   has_many :reviews
   validates :name, presence: true
   validates :price, presence: true
   validates :location, presence: true
-  validates :category, presence: true, inclusion: { in: ["Design", "Marketing", "Writing", "Video", "Music", "Programming", "Tech", "Bussiness", "Lifestyle"] }
+  validates :category, presence: true, inclusion: { in: CATEGORIES }
   validates :description, presence: true
   geocoded_by :address
   after_validation :geocode, if: :address_changed?
